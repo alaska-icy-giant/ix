f43fbdf16e0cdd77ac870c2ecfe6d1199e891e8f
Your Name <you@example.com>
2023-09-16 04:53:47 +0000

    ix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'ix',
   plugins: [
     vue({
       template: { transformAssetUrls },
